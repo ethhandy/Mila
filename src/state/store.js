c3b543ef1f161360a327bb9726fd9c22eb334afa
ethhandy <mariomyigold@gmail.com>
2023-06-08 18:50:19 +0200

    feat: zustand added

---
+++ src/state/store.js
@@ -0,0 +1,16 @@
+import { create } from 'zustand';
+import { persist } from 'zustand/middleware';
+
+const globalStore = create((set) => ({
+  width: window.innerWidth,
+  setWidth: (width) => set({ width }),
+
+  height: window.innerHeight,
+  setHeight: (height) => set({ height })
+}))
+
+const persistedGlobalStore = persist(globalStore, { name: 'GLOBAL_STORE' });
+
+const useGlobalStore = create(persistedGlobalStore);
+
+export default useGlobalStore;
