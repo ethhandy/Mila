9ac27f6ce264a129f644a69db349d12f40dae750
ethhandy <mariomyigold@gmail.com>
2023-06-11 08:47:56 +0200

    feat: tailwind.config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,3 @@
-/** @type {import('tailwindcss').Config} */
-const { colors } = require('tailwindcss/defaultTheme');
-
 module.exports = {
   content: ['./src/**/*.{js,jsx,ts,tsx}'],
   theme: {
@@ -9,32 +6,75 @@ module.exports = {
     colors: {
       black: '#292930',
       white: '#fff',
-      gray: {
-        ...colors.gray,
-        DEFAULT: '#999fae',
-        100: '#A0A0A0'
+
+      violet: {
+        DEFAULT: 'var(--color-violet-500)',
+        50: 'var(--color-violet-50)',
+        100: 'var(--color-violet-100)',
+        200: 'var(--color-violet-200)',
+        300: 'var(--color-violet-300)',
+        400: 'var(--color-violet-400)',
+        500: 'var(--color-violet-500)',
+        600: 'var(--color-violet-600)',
+        700: 'var(--color-violet-700)',
+        800: 'var(--color-violet-800)',
+        900: 'var(--color-violet-900)'
       },
-      blue: {
-        ...colors.blue,
-        DEFAULT: '#2522ba'
+
+      indigo: {
+        DEFAULT: 'var(--color-indigo-500)',
+        50: 'var(--color-indigo-50)',
+        100: 'var(--color-indigo-100)',
+        200: 'var(--color-indigo-200)',
+        300: 'var(--color-indigo-300)',
+        400: 'var(--color-indigo-400)',
+        500: 'var(--color-indigo-500)',
+        600: 'var(--color-indigo-600)',
+        700: 'var(--color-indigo-700)',
+        800: 'var(--color-indigo-800)',
+        900: 'var(--color-indigo-900)'
       },
-      purple: {
-        ...colors.purple,
-        100: '#5956e9',
-        200: '#5C5AED'
-      },
+
       pink: {
-        ...colors.pink,
-        100: '#FAB8C4',
-        200: '#FFBFCF'
+        DEFAULT: 'var(--color-pink-500)',
+        50: 'var(--color-pink-50)',
+        100: 'var(--color-pink-100)',
+        200: 'var(--color-pink-200)',
+        300: 'var(--color-pink-300)',
+        400: 'var(--color-pink-400)',
+        500: 'var(--color-pink-500)',
+        600: 'var(--color-pink-600)',
+        700: 'var(--color-pink-700)',
+        800: 'var(--color-pink-800)',
+        900: 'var(--color-pink-900)'
       },
+
       green: {
-        ...colors.green,
-        DEFAULT: '#90D0B1'
+        DEFAULT: 'var(--color-green-500)',
+        50: 'var(--color-green-50)',
+        100: 'var(--color-green-100)',
+        200: 'var(--color-green-200)',
+        300: 'var(--color-green-300)',
+        400: 'var(--color-green-400)',
+        500: 'var(--color-green-500)',
+        600: 'var(--color-green-600)',
+        700: 'var(--color-green-700)',
+        800: 'var(--color-green-800)',
+        900: 'var(--color-green-900)'
       },
-      red: {
-        ...colors.red,
-        DEFAULT: '#C75C6F'
+
+      gray: {
+        DEFAULT: 'var(--color-gray-500)',
+        50: 'var(--color-gray-50)',
+        100: 'var(--color-gray-100)',
+        200: 'var(--color-gray-200)',
+        300: 'var(--color-gray-300)',
+        400: 'var(--color-gray-400)',
+        500: 'var(--color-gray-500)',
+        600: 'var(--color-gray-600)',
+        700: 'var(--color-gray-700)',
+        800: 'var(--color-gray-800)',
+        900: 'var(--color-gray-900)'
       }
     }
   },
